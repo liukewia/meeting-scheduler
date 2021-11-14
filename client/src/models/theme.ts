5caecf72ae7847475d6b3cf3da06134f9fbcbe41
Yi Liu <liukewia@gmail.com>
2021-11-14 20:01:02 +0800

    update

--- client/src/models/theme.ts
+++ client/src/models/theme.ts
@@ -2,113 +2,67 @@ import { message } from 'antd';
 import { useState, useMemo, useCallback, useEffect } from 'react';
 import lightVars from '@/styles/light.json';
 import darkVars from '@/styles/dark.json';
+import { useLocalStorageState, useUpdateEffect } from 'ahooks';
 
 type ThemeTypes = 'light' | 'dark';
 
+function normRawTheme(rawStr: string) {
+  return (['light', 'dark'].includes(rawStr) ? rawStr : 'light') as ThemeTypes;
+}
+
 export default () => {
-  const [appTheme, setAppTheme] = useState<ThemeTypes>(
-    (() => {
-      const raw = localStorage.getItem('app-theme');
-      if (!raw || !['light', 'dark'].includes(raw)) {
-        const nonce = 'light';
-        localStorage.setItem('app-theme', nonce);
-        return nonce;
-      } else {
-        return localStorage.getItem('app-theme') as ThemeTypes;
-      }
-    })(),
+  const [appTheme, setAppTheme] = useLocalStorageState<ThemeTypes>(
+    'app-theme',
+    {
+      defaultValue: 'light',
+      serializer: (v) => normRawTheme(v),
+      deserializer: (v) => normRawTheme(v),
+    },
   );
 
-  const [customColors, setCustomColors] = useState<{ [key: string]: any }>(
-    (() => {
-      let raw = localStorage.getItem('app-colors');
-      let nonce;
-      if (!raw) {
-        nonce = {};
-        localStorage.setItem('app-colors', JSON.stringify(nonce));
-        return nonce;
-      } else {
-        try {
-          // fetch previous saved colors from local storage
-          nonce = JSON.parse(raw);
-        } catch (e) {
-          // prevent error thrown by json.parse
-          console.error('Failed parsing app colors from local storage');
-          nonce = {};
-        } finally {
-          return nonce;
-        }
-      }
-    })(),
-  );
+  const [customColors, setCustomColors] = useLocalStorageState<{
+    [key: string]: any;
+  }>('app-colors', {
+    defaultValue: {},
+  });
 
   const isLightTheme = useMemo(() => appTheme === 'light', [appTheme]);
 
+  // load less file with app-color & app-theme patched in the localstorage, only once after first render.
   useEffect(() => {
-    if (!['light', 'dark'].includes(appTheme)) {
-      setAppTheme('light');
-    }
+    const newVars = {
+      ...(isLightTheme ? lightVars : darkVars),
+      ...customColors,
+    };
+    window.less.modifyVars(newVars).catch((error) => {
+      message.error(`Failed to init theme`);
+    });
+  }, []);
+
+  useUpdateEffect(() => {
     const next = {
       ...(isLightTheme ? lightVars : darkVars),
       ...customColors,
-      '@white': '#fff',
-      '@black': '#000',
+      // '@white': '#fff',
+      // '@black': '#000',
     };
-
-    // window.less.modifyVars(next).catch((error) => {
-    //   message.error(`Failed to update theme`);
-    // });
-
-    requestIdleCallback(() =>
-      window.less.modifyVars(next).catch((error) => {
-        message.error(`Failed to update theme`);
-      }),
-    );
-
-    localStorage.setItem('app-theme', appTheme);
-    localStorage.setItem('app-colors', JSON.stringify(customColors));
+    window.less.modifyVars(next).catch((error) => {
+      message.error(`Failed to update theme`);
+    });
+    setCustomColors(customColors);
   }, [appTheme]);
 
-  useEffect(() => {
-    let raw = localStorage.getItem('app-colors');
-    let prev;
-    if (!raw) {
-      prev = {};
-    } else {
-      try {
-        prev = JSON.parse(raw);
-      } catch (e) {
-        console.error('Failed parsing app colors from local storage');
-        prev = {};
-      }
-    }
-
+  useUpdateEffect(() => {
     const next = {
       ...(isLightTheme ? lightVars : darkVars),
-      ...prev,
       ...customColors,
     };
-
-    // window.less.modifyVars(next).catch((error) => {
-    //   message.error(`Failed to update custom colors`);
-    // });
-
-    requestIdleCallback(() =>
-      window.less.modifyVars(next).catch((error) => {
-        message.error(`Failed to update custom colors`);
-      }),
-    );
-    localStorage.setItem('app-colors', JSON.stringify(customColors));
+    window.less.modifyVars(next).catch((error) => {
+      message.error(`Failed to update custom colors`);
+    });
   }, [customColors]);
 
   const resetAllThemes = useCallback(() => {
-    requestIdleCallback(() =>
-      window.less.modifyVars(lightVars).catch((error) => {
-        message.error(`Failed to reset theme`);
-      }),
-    );
-    localStorage.setItem('app-theme', 'light');
-    localStorage.setItem('app-colors', '{}');
     setAppTheme('light');
     setCustomColors({});
   }, []);
