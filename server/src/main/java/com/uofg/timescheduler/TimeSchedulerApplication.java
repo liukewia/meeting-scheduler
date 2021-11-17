c652d0c5c51fc13f1dfd32d186c1151bac5fa9e9
Yi Liu <liukewia@gmail.com>
2021-11-17 15:52:16 +0800

    update account profile

--- server/src/main/java/com/uofg/timescheduler/TimeSchedulerApplication.java
+++ server/src/main/java/com/uofg/timescheduler/TimeSchedulerApplication.java
@@ -1,5 +1,7 @@
 package com.uofg.timescheduler;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
@@ -11,6 +13,11 @@ public class TimeSchedulerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TimeSchedulerApplication.class, args);
+    }
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 
 }
